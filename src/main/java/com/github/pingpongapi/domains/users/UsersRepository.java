96897796356fd4d0b929a4f3eaeeaa4a998ef0e0
Darren Shin <hshin@catalyte.io>
2021-09-23 10:42:10 -0600

    debugged

--- src/main/java/com/github/pingpongapi/domains/users/UsersRepository.java
+++ src/main/java/com/github/pingpongapi/domains/users/UsersRepository.java
@@ -1,7 +1,5 @@
 package com.github.pingpongapi.domains.users;
 
-import java.util.List;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
